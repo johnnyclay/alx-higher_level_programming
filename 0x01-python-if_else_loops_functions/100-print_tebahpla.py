cb25c29fc23a8bd3face2c88844628b4d008e27d
johnnyclay <kidusyohannes456@gmail.com>
2022-06-07 15:22:08 +0000

    else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(-122, -96):
-    if i % 2 == 0:
-        let = chr(i * -1)
-    else:
-        let = chr((i * -1) - 32)
-    print("{}".format(let), end="")
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
